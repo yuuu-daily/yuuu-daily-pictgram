c6c0bf218951c1a853522a4e8ad3c3e5e16dfa55
yuichi <y.ichityzy@gmail.com>
2022-02-17 13:12:29 +0900

    java24

---
+++ src/main/java/com/example/pictgram/form/TopicForm.java
@@ -0,0 +1,34 @@
+package com.example.pictgram.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.pictgram.validation.constraints.ImageByte;
+import com.example.pictgram.validation.constraints.ImageNotEmpty;
+
+import lombok.Data;
+
+@Data
+public class TopicForm {
+
+    private Long id;
+
+    private Long userId;
+
+    @ImageNotEmpty
+    @ImageByte(max = 2000000)
+    private MultipartFile image;
+
+    private String imageData;
+
+    private String path;
+
+    @NotEmpty
+    @Size(max = 1000)
+    private String description;
+
+    private UserForm user;
+
+}
