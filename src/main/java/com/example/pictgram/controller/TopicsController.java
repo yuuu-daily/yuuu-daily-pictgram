ae6ebdd52e115750e073a479b49d2c3ae70a44b4
yuichi <y.ichityzy@gmail.com>
2022-02-22 14:05:18 +0900

    java 26

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -34,9 +34,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.pictgram.entity.Comment;
 import com.example.pictgram.entity.Favorite;
 import com.example.pictgram.entity.Topic;
 import com.example.pictgram.entity.UserInf;
+import com.example.pictgram.form.CommentForm;
 import com.example.pictgram.form.FavoriteForm;
 import com.example.pictgram.form.TopicForm;
 import com.example.pictgram.form.UserForm;
@@ -83,6 +85,7 @@ public class TopicsController {
         modelMapper.getConfiguration().setAmbiguityIgnored(true);
         modelMapper.typeMap(Topic.class, TopicForm.class).addMappings(mapper -> mapper.skip(TopicForm::setUser));
         modelMapper.typeMap(Topic.class, TopicForm.class).addMappings(mapper -> mapper.skip(TopicForm::setFavorites));
+        modelMapper.typeMap(Topic.class, TopicForm.class).addMappings(mapper -> mapper.skip(TopicForm::setComments));
         modelMapper.typeMap(Favorite.class, FavoriteForm.class).addMappings(mapper -> mapper.skip(FavoriteForm::setTopic));
         
         boolean isImageLocal = false;
@@ -121,6 +124,14 @@ public class TopicsController {
         	}
         }
         form.setFavorites(favorites);
+        
+        List<CommentForm> comments = new ArrayList<CommentForm>();
+        
+        for (Comment commentEntity : entity.getComments()) {
+        	CommentForm comment = modelMapper.map(commentEntity, CommentForm.class);
+        	comments.add(comment);
+        }
+        form.setComments(comments);
 
         return form;
     }
