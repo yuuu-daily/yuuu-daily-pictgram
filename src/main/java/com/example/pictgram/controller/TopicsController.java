abb1eb0d066aa404eb41a0a6cc33775e1de78ad1
yuichi <y.ichityzy@gmail.com>
2022-03-17 07:38:00 +0900

    java 33

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +23,13 @@ import org.apache.sanselan.common.IImageMetadata;
 import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
 import org.apache.sanselan.formats.tiff.TiffImageMetadata.GPSInfo;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,6 +41,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thymeleaf.context.Context;
@@ -47,6 +51,7 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
+import com.example.pictgram.bean.TopicCsv;
 import com.example.pictgram.entity.Comment;
 import com.example.pictgram.entity.Favorite;
 import com.example.pictgram.entity.Topic;
@@ -58,6 +63,8 @@ import com.example.pictgram.form.UserForm;
 import com.example.pictgram.repository.TopicRepository;
 import com.example.pictgram.service.S3Wrapper;
 import com.example.pictgram.service.SendMailService;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 @Controller
 public class TopicsController {
@@ -318,6 +325,20 @@ public class TopicsController {
         } catch (ImageReadException | IOException e) {
             log.warn(e.getMessage(), e);
         }
+    }
+    
+    @RequestMapping(value = "/topics/topic.csv", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+            + "; charset=UTF-8; Content-Disposition: attachment")
+    @ResponseBody
+    public Object downloadCsv() throws IOException {
+        Iterable<Topic> topics = repository.findAll();
+        Type listType = new TypeToken<List<TopicCsv>>() {
+        }.getType();
+        List<TopicCsv> csv = modelMapper.map(topics, listType);
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(TopicCsv.class).withHeader();
+
+        return mapper.writer(schema).writeValueAsString(csv);
     }
 
 }
